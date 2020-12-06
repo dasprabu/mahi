f0121ce39a33f45a821ab4a85883417be61a5edc
Prabu <dasprabu@gmail.com>
2020-12-06 13:03:05 +0800

    Database Migrations

---
+++ src/CmdLine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CmdLine.DataAccess;
+
+namespace CmdLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.RunMigrations();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
